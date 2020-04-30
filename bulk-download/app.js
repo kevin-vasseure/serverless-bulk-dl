418c673b2186fa637b294e80d7827dc5f6df3fe6
Kevin Vasseure <kevin.vasseure@asics.com>
2020-04-30 15:01:31 +0900

    remove the master function (useless) - fix tests

--- bulk-download/app.js
+++ bulk-download/app.js
@@ -14,35 +14,54 @@ const fileSize = require('file-size')
  * @param context
  * @returns {Promise<{body: string, statusCode: number}|*>}
  */
-exports.apiHandler = async (event, context) => {
+async function apiHandler(event, context) {
     let response
+    let payload
+    let jsonSchemaErrors = []
 
     console.log(`received event ${event}`)
-    let payload = JSON.parse(event.body)
-    let validation = validate(payload, payloadSchema)
-
-    if (validation.errors.length > 0) {
-        console.warn("invalid payload " + JSON.stringify(validation.errors, null, 2))
+    try {
+        payload = JSON.parse(event.body)
+        let validation = validate(payload, payloadSchema)
+        jsonSchemaErrors = validation.errors
+    } catch (error) {
+        console.warn("invalid payload : " + error.message)
         response = {
             'statusCode': 400,
             'body': JSON.stringify({
-                message: validation.errors,
+                message: error.message,
             })
         }
-    } else {
+    }
+
+    if (!response && jsonSchemaErrors.length > 0) {
+        console.warn("invalid payload : " + JSON.stringify(jsonSchemaErrors))
+        response = {
+            'statusCode': 400,
+            'body': JSON.stringify({
+                message: jsonSchemaErrors,
+            })
+        }
+    }
+
+    if (!response) {
         try {
             payload = await enrichPayload(payload)
-            console.log("sending enriched payload to " + process.env.ZIP_MASTER_FUNCTION + " => " + JSON.stringify(payload, null, 2))
-            let lambda = new AWS.Lambda()
-            await lambda.invoke({
-                FunctionName: process.env.ZIP_MASTER_FUNCTION,
-                InvocationType: 'Event',
-                Payload: JSON.stringify(payload)
-            }).promise()
+            payload = await triggerZipLambdas(payload)
+
             response = {
                 'statusCode': 200,
                 'body': JSON.stringify({
-                    message: 'expected bulk download size: ' + fileSize(payload.totalSize).human(),
+                    message: {
+                        totalSize: fileSize(payload.totalSize).human(),
+                        zipList: payload.zipList.map((zip) => {
+                            return {
+                                assets: zip.assets,
+                                zipName: zip.zipName,
+                                size: fileSize(zip.size).human()
+                            }
+                        })
+                    }
                 })
             }
         } catch (err) {
@@ -55,6 +74,7 @@ exports.apiHandler = async (event, context) => {
 
 /**
  * Will enrich the payload with the size of the assets
+ * through HEAD requests run in parrallel
  * @param payload
  * @returns {Promise<void>}
  */
@@ -72,48 +92,55 @@ async function enrichPayload(payload) {
 /**
  * Check the assets size, split downloads into different archives
  * @param payload
- * @returns {Promise<void>}
+ * @returns {Promise<[]>}
  */
-exports.zipMasterHandler = async (payload) => {
+async function triggerZipLambdas(payload) {
     console.log("received event " + JSON.stringify(payload, null, 2))
 
     const maxSize = process.env.ARCHIVE_SIZE || 1000000000
     const lambda = new AWS.Lambda()
-    const lambdaResponses = []
+    const invocationList = []
 
     //make a hash id out of filenames and sizes
     const data = payload.assets.reduce((a, b) => a.name + a.size + b.name + b.size, {name: "", size: ""})
     const hashID = "download_" + crypto.createHash('md5').update(data).digest("hex")
 
     let sizeCounter = 0
-    let assetsList = []
-    let zipCount = 0
+    let assetsAccumulator = []
+    let zipCount = 1
+    let zipList = []
 
     //loop on assets url
     payload.assets.forEach((asset, index) => {
         sizeCounter += asset.size
-        assetsList.push(asset)
-        if (sizeCounter > maxSize || (index + 1 === payload.assets.length)) { //until we reach our size threshold
-            const zipName = (payload.assets > 1) ? hashID + "-" + zipCount++ + ".zip" : hashID + ".zip"
-            console.log("calling zip function " + process.env.ZIP_FUNCTION + " => " + JSON.stringify(assetsList, null, 2))
+        assetsAccumulator.push(asset)
+        if (sizeCounter > maxSize || (index + 1 === payload.assets.length)) { //until we reach our size threshold or end of array
+            const zipName = (sizeCounter < payload.totalSize || zipCount > 1) ? hashID + "-" + zipCount++ + ".zip" : hashID + ".zip"
+            console.log("calling zip function " + process.env.ZIP_FUNCTION + " => " + JSON.stringify(assetsAccumulator))
             console.log("estimated size " + fileSize(sizeCounter).human())
             //send a sublist of assets to a zip lambda
-            lambdaResponses.push(lambda.invoke({
+            const sublist = {assets: assetsAccumulator, zipName: zipName, size: sizeCounter}
+            zipList.push(sublist)
+            //call zip lambda
+            let invocation = lambda.invoke({
                 FunctionName: process.env.ZIP_FUNCTION,
-                Payload: JSON.stringify({assets: assetsList, zipName: zipName})
-            }).promise())
+                InvocationType: 'Event',
+                Payload: JSON.stringify(sublist)
+            }).promise()
+            invocationList.push(invocation)
             //reset asset list and size counter
-            assetsList = []
+            assetsAccumulator = []
             sizeCounter = 0
         }
     })
 
-    //TODO what to do with the response?
-    await Promise.all(lambdaResponses);
+    await Promise.all(invocationList)
+    payload.zipList = zipList
+    return Promise.resolve(payload)
 }
 
 /**
- * Synchronous function that streams binary data from URL
+ * streams binary data from URL
  * pipe it into archiver then directly to s3
  *
  * inspired from here https://dev.to/lineup-ninja/zip-files-on-s3-with-aws-lambda-and-node-1nm1
@@ -121,8 +148,8 @@ exports.zipMasterHandler = async (payload) => {
  * @param payload
  * @returns file location
  */
-exports.zipHandler = async (payload) => {
-    console.log("received event " + JSON.stringify(payload, null, 2))
+async function zipHandler(payload) {
+    console.log("received event " + JSON.stringify(payload))
 
     const s3 = new AWS.S3()
     const streamPassThrough = new stream.PassThrough()
@@ -150,10 +177,19 @@ exports.zipHandler = async (payload) => {
     archive.pipe(streamPassThrough)
     for (let asset of payload.assets) {
         try {
-            let request = await axios({url:  asset.url, responseType: 'stream'})
+            let request = await axios({url: asset.url, responseType: 'stream'})
             archive.append(request.data, {name: asset.name})
-        } catch (e) {throw e}
+        } catch (e) {
+            throw e
+        }
     }
     archive.finalize()
     return s3Upload.promise()
+}
+
+module.exports = {
+    apiHandler: apiHandler,
+    enrichPayload: enrichPayload,
+    triggerZipLambdas: triggerZipLambdas,
+    zipHandler: zipHandler
 }
--- bulk-download/tests/unit/handler.test.js
+++ bulk-download/tests/unit/handler.test.js
@@ -1,7 +1,10 @@
 const app = require('../../app.js')
-const event = require('../../../events/api_event.json')
-const wrong_event = require('../../../events/api_event_bad_payload.json')
-const payload = require('../../../events/zip_master_payload.json')
+const eventBadSchema = require('../../../stubs/api_event_bad_schema.json')
+const eventBadJson = require('../../../stubs/api_event_bad_json.json')
+const payloadRaw = require('../../../stubs/payload_raw.json')
+const payloadEnriched = require('../../../stubs/payload_enriched.json')
+const payloadEnrichedOneAsset = require('../../../stubs/payload_enriched_one_asset.json')
+const event = require('../../../stubs/api_event.json')
 
 jest.mock('axios', () => {
     const response = {headers: {'content-length': '1000'}}
@@ -32,29 +35,58 @@ jest.mock('archiver', () => {
 })
 
 let context
-describe('Test api handler', function () {
-    it('verifies successful response', async () => {
-        const result = await app.apiHandler(event, context)
-        expect(result.statusCode).toEqual(200)
-        let response = JSON.parse(result.body)
-        expect(response.message).toEqual("expected bulk download size: 1.95 KiB")
-    })
-    it('verifies wrong payload', async () => {
-        const result = await app.apiHandler(wrong_event, context)
+describe('api handler validation', function () {
+    it('verifies that payload with wrong schema is handled', async () => {
+        const result = await app.apiHandler(eventBadSchema, context)
         expect(result.statusCode).toEqual(400)
-        JSON.parse(result.body)
+        const responseBody = JSON.parse(result.body);
+        expect(responseBody.message[0].message).toContain('requires property')
+    })
+    it('verifies that payload with incorrect json is handled', async () => {
+        const result = await app.apiHandler(eventBadJson, context)
+        expect(result.statusCode).toEqual(400)
+        const responseBody = JSON.parse(result.body);
+        expect(responseBody.message).toContain('Unexpected token')
     })
 })
 
-describe('Test zip master handler', function () {
-    it('verifies it starts', async () => {
-        await app.zipMasterHandler(payload)
+describe('enrich payload function test', function () {
+    it('verifies that payload is enriched with asset size and total size', async () => {
+        const enrichedPayload = await app.enrichPayload(payloadRaw)
+        expect(enrichedPayload.assets[0].size).toEqual(1000)
+        expect(enrichedPayload.totalSize).toEqual(4000)
+    })
+})
+
+describe('tests on the function that triggers zips', function () {
+    it('verifies how it splits bulk dls', async () => {
+        let payload = await app.triggerZipLambdas(payloadEnriched)
+        expect(payload.zipList.length).toEqual(2)
+        expect(payload.zipList[0].zipName).toMatch(/.*-1\.zip$/)
+        expect(payload.zipList[0].assets.length).toEqual(3)
+        expect(payload.zipList[1].zipName).toMatch(/.*-2\.zip$/)
+        expect(payload.zipList[1].assets.length).toEqual(1)
+    })
+    it('verifies it doesnt split when only one asset', async () => {
+        let payload = await app.triggerZipLambdas(payloadEnrichedOneAsset)
+        expect(payload.zipList.length).toEqual(1)
+        expect(payload.zipList[0].zipName).toMatch(/.*(?<!-\d)\.zip$/)
+    })
+})
+
+describe('api handler response', function () {
+    it('verifies that payload with wrong schema is handled', async () => {
+        const result = await app.apiHandler(event, context)
+        expect(result.statusCode).toEqual(200)
+        const responseBody = JSON.parse(result.body);
+        expect(responseBody.message.totalSize).toEqual("1.95 KiB")
+        expect(responseBody.message.zipList.length).toEqual(1)
     })
 })
 
 describe('Test zip handler', function () {
     it('verifies it starts', async () => {
-        await app.zipHandler(payload)
+        await app.zipHandler(payloadEnriched)
     })
 })
 
